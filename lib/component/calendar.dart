b6c39d4939b27f6b8454c69b868c439079902695
김현우 <wry1004@cocen.com>
2023-09-25 08:28:24 +0900

    add bottomSheet

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -2,15 +2,13 @@ import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({super.key});
+class Calendar extends StatelessWidget {
+  final DateTime? selectedDay;
+  final DateTime focusedDay;
+  final OnDaySelected? onDaySelected;
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
-  DateTime? selectedDay;
+  const Calendar(
+      {required this.selectedDay, required this.focusedDay,required this.onDaySelected, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +22,8 @@ class _CalendarState extends State<Calendar> {
       fontWeight: FontWeight.w700,
     );
     return TableCalendar(
-      focusedDay: DateTime.now(),
+      locale: 'ko_KR',
+      focusedDay: focusedDay,
       firstDay: DateTime(1800),
       lastDay: DateTime(3000),
       headerStyle: HeaderStyle(
@@ -33,28 +32,19 @@ class _CalendarState extends State<Calendar> {
         titleTextStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 16.0),
       ),
       calendarStyle: CalendarStyle(
-        isTodayHighlighted: false,
-        defaultDecoration: defaultBoxDeco,
-        weekendDecoration: defaultBoxDeco,
-        selectedDecoration:  BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(6.0),
-          border: Border.all(
-            color: PRIMARY_COLOR,
-            width: 1.0
+          isTodayHighlighted: false,
+          defaultDecoration: defaultBoxDeco,
+          weekendDecoration: defaultBoxDeco,
+          selectedDecoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(6.0),
+            border: Border.all(color: PRIMARY_COLOR, width: 1.0),
           ),
-        ),
-        defaultTextStyle: defaultTextStyle,
-        weekendTextStyle: defaultTextStyle,
-        selectedTextStyle: defaultTextStyle.copyWith(
-          color: PRIMARY_COLOR
-        )
-      ),
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-        setState(() {
-          this.selectedDay = selectedDay;
-        });
-      },
+          outsideDecoration: BoxDecoration(shape: BoxShape.rectangle),
+          defaultTextStyle: defaultTextStyle,
+          weekendTextStyle: defaultTextStyle,
+          selectedTextStyle: defaultTextStyle.copyWith(color: PRIMARY_COLOR)),
+      onDaySelected: onDaySelected,
       selectedDayPredicate: (DateTime date) {
         if (selectedDay == null) return false;
 
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,17 +1,90 @@
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 
 import '../component/calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: renderFloatingActionButton(),
       body: SafeArea(
         child: Column(
-          children: [Calendar()],
+          children: [
+            Calendar(
+              selectedDay: selectedDay,
+              focusedDay: focusedDay,
+              onDaySelected: OnDaySelected,
+            ),
+            SizedBox(height: 8.0),
+            TodayBanner(selectedDay: selectedDay, scheduleCount: 3),
+            SizedBox(
+              height: 8.0,
+            ),
+            _ScheduleList()
+          ],
         ),
+      ),
+    );
+  }
+
+  FloatingActionButton renderFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (_) {
+              return ScheduleBottomSheet();
+            });
+      },
+      backgroundColor: PRIMARY_COLOR,
+      child: Icon(Icons.add),
+    );
+  }
+
+  OnDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+      this.focusedDay = selectedDay;
+    });
+  }
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.separated(
+            itemCount: 7,
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 8.0);
+            },
+            itemBuilder: (context, index) {
+              return ScheduleCard(
+                startTime: 12,
+                endTime: 14,
+                content: '프로그래밍 공부하기',
+                color: Colors.red,
+              );
+            }),
       ),
     );
   }
